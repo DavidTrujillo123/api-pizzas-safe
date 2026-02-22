b36027bea1de0fbe8696f27b82f71e5452778bd3
David Trujillo <davot098@gmail.com>
2026-02-22 17:51:14 -0500

    Agregado geoip para obtener la localizacion.

--- src/devices/devices.service.ts
+++ src/devices/devices.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import * as geoip from 'geoip-lite';
 import { Device } from './entities/device.entity';
 import { User } from '../users/entities/user.entity';
 
@@ -16,10 +17,27 @@ export class DevicesService {
     ipAddress: string,
     deviceType: string,
   ): Promise<Device> {
+    // Normalizar IP (en local ::1 o ::ffff:127.0.0.1)
+    const cleanIp = ipAddress.includes('::ffff:')
+      ? ipAddress.split('::ffff:')[1]
+      : ipAddress === '::1'
+        ? '127.0.0.1'
+        : ipAddress;
+
+    const geo = geoip.lookup(cleanIp);
+    let location = 'Unknown Location';
+
+    if (geo) {
+      location = `${geo.city || 'Unknown City'}, ${geo.country}`;
+    } else if (cleanIp === '127.0.0.1') {
+      location = 'Localhost';
+    }
+
     const newDevice = this.deviceRepository.create({
       user,
-      ipAddress,
+      ipAddress: cleanIp,
       deviceType,
+      location,
     });
 
     return await this.deviceRepository.save(newDevice);
